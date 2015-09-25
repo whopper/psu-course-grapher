58c5424066ce3d533dc5e43922e45406235bb102
Will Hopper <whopper@puppetlabs.com>
2015-09-24 20:10:28 -0700

    Initial commit
    This commit adds basic data-grabbing functionality classes
    which pull down available course data from PSU's smart
    bulletin site. This is a work in progress and proof of
    concept.

---
+++ course.rb
@@ -0,0 +1,11 @@
+class Course
+  attr_accessor :name, :url, :description, :prereqs, :credits
+
+  def initialize
+    @prereqs = Array.new
+  end
+
+  def add_prereq(course)
+    @prereqs << course
+  end
+end
